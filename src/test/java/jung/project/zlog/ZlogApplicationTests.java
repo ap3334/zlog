d8bcd3f03e378bda980c8c770b5e499b00ec679f
ap3334 <chwil2989@naver.com>
2022-08-17 16:35:32 +0900

    초기 세팅

---
+++ src/test/java/jung/project/zlog/ZlogApplicationTests.java
@@ -0,0 +1,13 @@
+package jung.project.zlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
