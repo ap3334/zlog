1874c5627ab30ac380dfbba9ee27761f9d83c27a
ap3334 <chwil2989@naver.com>
2022-08-31 22:54:57 +0900

    [#16] 회원가입 시 DB에 값 저장(비밀번호는 해시화)

---
+++ src/main/java/jung/project/zlog/controller/user/UserController.java
@@ -0,0 +1,36 @@
+package jung.project.zlog.controller.user;
+
+import jung.project.zlog.dto.user.UserDto;
+import jung.project.zlog.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/join")
+    public String joinPage() {
+
+        return "/user/join";
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<Integer> join(@RequestBody UserDto userDto) {
+
+        int id = userService.saveUser(userDto);
+
+        return new ResponseEntity<>(200, HttpStatus.OK);
+
+    }
+
+}
---
+++ src/main/java/jung/project/zlog/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package jung.project.zlog.repository.user;
+
+import jung.project.zlog.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
