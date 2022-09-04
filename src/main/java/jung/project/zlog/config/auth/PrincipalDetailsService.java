a8b3207fa56007fce045414cf48a6b28c4a3f03a
ap3334 <chwil2989@naver.com>
2022-09-04 21:31:44 +0900

    [#17] 시큐리티 사용해 로그인 기능 구현

---
+++ src/main/java/jung/project/zlog/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,23 @@
+package jung.project.zlog.config.auth;
+
+import jung.project.zlog.entity.user.User;
+import jung.project.zlog.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
+
+        return new PrincipalDetails(user);
+    }
+}
